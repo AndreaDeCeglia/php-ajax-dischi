e4cc5c1cc968a98167b416a0c16aef06e85b24e2
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-15 18:54:37 +0100

    js' methods written

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ var app = new Vue({
 
     data: {
         jsonArray : [],
+        genresArray: [],
+        genre: '',
     },
 
     mounted(){
@@ -11,10 +13,24 @@ var app = new Vue({
                 console.log(`i'm response.data`, res.data);
                 this.jsonArray = res.data;
                 console.log(`i'm jsonArray`, this.jsonArray);
+                //cicle for filtering music's genres and push'em
+                //in proper array for filling select's options
+                this.jsonArray.forEach((element) => {
+                    console.log(`i'm the element in jsonArray`, element);
+                    //condition for not reapiting the same genre
+                    if(!this.genresArray.includes(element.genre)){
+                        this.genresArray.push(element.genre);
+                    }
+                })
             })
     },
 
     methods: {
-
+        selectGenre(){
+            axios.get(`./api/index.php?tipo=${this.genre}`)
+                .then((res) => {
+                    this.jsonArray = res.data;
+                })
+        }
     }  
 })
