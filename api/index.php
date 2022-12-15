349358386a0f63715bd891da767c8156f2351e00
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-15 15:50:40 +0100

    db translated

---
+++ api/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+include_once __DIR__ . '/../db/index.php';
+
+$emptyCD = [];
+
+foreach($database as element){
+    $emptyCD[] = element;
+};
+
+header('Content-type: application/json');
+
+echo json_encode( $emptyCD );
+
+?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 var app = new Vue({
     el: '#app',
+
     data: {
 
     },
-    mounted(){
 
+    mounted(){
+        axios.get('./api/index.php')
+            .then((response) => {
+                console.log(`i'm response.data`, this.response.data)
+            })
     },
+
     methods: {
-        
-    }
+
+    }  
 })
